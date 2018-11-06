03f51e65f6bbfbff113773e7ef185585ea3a3c05
tien <tienoho@gmail.com>
2018-11-06 16:40:41 +0700

    bố tiến

--- src/com/btl/quanlythuvien/controler/MarcBean2.java
+++ src/com/btl/quanlythuvien/controler/MarcBean2.java
@@ -4,43 +4,44 @@ public class MarcBean2 {
 
     // bib
     private String id;
-
-    // 005 ngay thang nam
-    private String ye005;
-    private String da005;
-    private String mo005;
-
+    // LDR
+    private String LDR;
+    // 008
+    private String sp008;
+    // 041
+    private String sp041;
     // 082 $$a $$b ma xep gia || DDC
     private String sp082;
     // 100 $$a $$e  ||
     private String sp100;
-    //private String in082;
     // 245 $$a .... || $$c
     private String sp245;
-
     // 260 $$a imprint || $$c year
     private String sp260;
     //300
     private String sp300;
     //650
     private String sp650;
-    // 852 $$b localtion
-    private String lo852;
-
-    // 916 linh vuc
-    private String ty916;
-
+    // 911 linh vuc
+    private String ty911;
+    // 915 linh vuc
+    private String ty925;
     // 927 loai hinh tai lieu
     private String fi927;
 
-    public MarcBean2(String id, String sp082, String sp100, String sp245, String sp260, String sp300, String sp650, String fi927) {
+    public MarcBean2(String id, String LDR, String sp008, String sp041, String sp082, String sp100, String sp245, String sp260, String sp300, String sp650, String ty911, String ty925, String fi927) {
         this.id = id;
+        this.LDR = LDR;
+        this.sp008 = sp008;
+        this.sp041 = sp041;
         this.sp082 = sp082;
         this.sp100 = sp100;
         this.sp245 = sp245;
         this.sp260 = sp260;
         this.sp300 = sp300;
         this.sp650 = sp650;
+        this.ty911 = ty911;
+        this.ty925 = ty925;
         this.fi927 = fi927;
     }
 
@@ -48,36 +49,48 @@ public class MarcBean2 {
         return id;
     }
 
-    public String getYe005() {
-        return ye005;
+    public String getLDR() {
+        return LDR;
     }
 
-    public String getDa005() {
-        return da005;
+    public String getSp008() {
+        return sp008;
     }
 
-    public String getMo005() {
-        return mo005;
+    public String getSp041() {
+        return sp041;
     }
 
+    public String getSp082() {
+        return sp082;
+    }
 
     public String getSp100() {
         return sp100;
     }
 
+    public String getSp245() {
+        return sp245;
+    }
+
+    public String getSp260() {
+        return sp260;
+    }
 
     public String getSp300() {
         return sp300;
     }
 
-
-
-    public String getLo852() {
-        return lo852;
+    public String getSp650() {
+        return sp650;
     }
 
-    public String getTy916() {
-        return ty916;
+    public String getTy911() {
+        return ty911;
+    }
+
+    public String getTy925() {
+        return ty925;
     }
 
     public String getFi927() {
@@ -87,19 +100,19 @@ public class MarcBean2 {
     @Override
     public String toString() {
         return "MarcBean{" +
-                "id='" + id + '\'' +"\n"+
-                ", ye005='" + ye005 + '\'' +"\n"+
-                ", da005='" + da005 + '\'' +"\n"+
-                ", mo005='" + mo005 + '\'' +"\n"+
-                ", Sp082='" + sp082 + '\'' +"\n"+
-                ", Sp100='" + sp100 + '\'' +"\n"+
-                ", Sp245='" + sp245 + '\'' +"\n"+
-                ", Sp260='" + sp260 + '\'' +"\n"+
-                ", au300='" + sp300 + '\'' +"\n"+
-                ", au650='" + sp650 + '\'' +"\n"+
-                ", lo852='" + lo852 + '\'' +"\n"+
-                ", ty916='" + ty916 + '\'' +"\n"+
-                ", fi927='" + fi927 + '\'' +"\n"+
+                "LDR='" + LDR + '\'' + "\n" +
+                "da001='" + id + '\'' + "\n" +
+                "da008='" + sp008 + '\'' + "\n" +
+                "da041='" + sp041 + '\'' + "\n" +
+                "Sp082='" + sp082 + '\'' + "\n" +
+                "Sp100='" + sp100 + '\'' + "\n" +
+                "Sp245='" + sp245 + '\'' + "\n" +
+                "Sp260='" + sp260 + '\'' + "\n" +
+                "sp300='" + sp300 + '\'' + "\n" +
+                "sp650='" + sp650 + '\'' + "\n" +
+                "ty911='" + ty911 + '\'' + "\n" +
+                "ty925='" + ty925 + '\'' + "\n" +
+                "fi927='" + fi927 + '\'' + "\n" +
                 '}';
     }
 }
