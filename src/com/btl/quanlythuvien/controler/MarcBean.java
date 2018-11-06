b943e7daf9d919ce7dfab1c2656c0eb4ca5c33a6
tien <tienoho@gmail.com>
2018-11-06 15:36:32 +0700

    bo tien

--- src/com/btl/quanlythuvien/controler/MarcBean.java
+++ src/com/btl/quanlythuvien/controler/MarcBean.java
@@ -13,7 +13,9 @@ public class MarcBean {
     // 082 $$a $$b ma xep gia || DDC
     private String cn082;
     private String in082;
-
+    // 100 $$a $$e  ||
+    private String sp100;
+    //private String in082;
     // 245 $$a .... || $$c
     private String ti245;
     private String au245;
@@ -21,6 +23,8 @@ public class MarcBean {
     // 260 $$a imprint || $$c year
     private String im260;
     private String ye260;
+    //300
+    private String sp300;
 
     // 852 $$b localtion
     private String lo852;
@@ -31,12 +35,14 @@ public class MarcBean {
     // 927 loai hinh tai lieu
     private String fi927;
 
-    public MarcBean(String id, String cn082, String in082, String ti245, String au245, String im260, String ye260, String lo852, String ty916, String fi927) {
+    public MarcBean(String id, String cn082, String in082,String sp100, String ti245, String au245, String sp300,String im260, String ye260, String lo852, String ty916, String fi927) {
         this.id = id;
         this.cn082 = cn082;
         this.in082 = in082;
+        this.sp100=sp100;
         this.ti245 = ti245;
         this.au245 = au245;
+        this.sp300=sp300;
         this.im260 = im260;
         this.ye260 = ye260;
         this.lo852 = lo852;
@@ -67,6 +73,9 @@ public class MarcBean {
     public String getIn082() {
         return in082;
     }
+    public String getSp100() {
+        return sp100;
+    }
 
     public String getTi245() {
         return ti245;
@@ -74,6 +83,9 @@ public class MarcBean {
 
     public String getAu245() {
         return au245;
+    }
+    public String getSp300() {
+        return sp300;
     }
 
     public String getIm260() {
@@ -105,8 +117,10 @@ public class MarcBean {
                 ", mo005='" + mo005 + '\'' +
                 ", cn082='" + cn082 + '\'' +
                 ", in082='" + in082 + '\'' +
+                ", Sp100='" + sp100 + '\'' +
                 ", ti245='" + ti245 + '\'' +
                 ", au245='" + au245 + '\'' +
+                ", au300='" + sp300 + '\'' +
                 ", im260='" + im260 + '\'' +
                 ", ye260='" + ye260 + '\'' +
                 ", lo852='" + lo852 + '\'' +
