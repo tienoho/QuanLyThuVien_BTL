dda570d29e50913d6ba86d3e771898e0bfcc01a8
comTien <tienoho@gmail.com>
2018-11-10 09:02:58 +0700

    oc ccho

--- src/com/btl/quanlythuvien/model/DBConnection.java
+++ src/com/btl/quanlythuvien/model/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
     private Connection conn = null;
     private final String user = "root";
    // private final String pass = "root";
-    private final String pass = "tre150413";
+    private final String pass = "";
   //  private final String cs = "jdbc:mysql://192.168.1.221:3306/QuanLyThuVien";
     private final String cs = "jdbc:mysql://localhost:3306/QuanLyThuVien";
 
